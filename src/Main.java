5b0de2c67dcc901b3d33c3be6adc91630228c3ee
Tamar Bediashvili <tamar.bediashvili@rockit.ge>
2022-04-26 22:19:37 +0400

    Assignment 8

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //TODO Task N1 isValid
+
+        //TODO Task N2 create class Queue which implements QueueInterface
+        //Don't use java java.util.Queue
+    }
+
+    //TODO Task N1
+    private static boolean isValid(String s) {
+        //return if it's valid parentheses (https://leetcode.com/problems/valid-parentheses/)
+        return false;
+    }
+}
---
+++ src/QueueInterface.java
@@ -0,0 +1,10 @@
+public interface QueueInterface {
+
+    void push(int elem);
+
+    int pop();
+
+    int peek();
+
+    int size();
+}
